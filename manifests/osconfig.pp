60eac4b57007ef1859aa371cdb38f14b77d49ef4
michaeldkelly <michaeldkelly@gmail.com>
2015-09-22 23:33:42 -0400

    nopasswd for wheel group
    for service account

--- manifests/osconfig.pp
+++ manifests/osconfig.pp
@@ -22,6 +22,15 @@ class pwk::osconfig {
     type => 'rsa',
     key =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQCvazcy/ZyZCfsHljtK76VZXplMylQweipO4tHs9bUdQM7Jnl5//6akwB2lj1bQbC4n3gs3evI6ZOWPL8tnfLdtR0zQAWuGIfpEX9bbbzUWsm5bKoaglrhTWKToDC1qQcMEe7nLtqpAmPw95lwJWyZZv1MQKAzDuXkTwJUebytLn3Rji1uRSNpV8W7RTOvbu5PFFDNAJYekNKRnvuCqZeU8e8WoyTXkswN0OLblXp06x94zsyBUN9ETqmz5rck65QcGYtaGBDF+/ghk5iUwt5MKZzLYlC8zWgp4fB/aq7yiZxFFGigM5NvZlOk6hpnfw2NILPWJ5bzQJygtdDlatVWz',
   }
+  augeas { 'sudo':
+    context => '/files/etc/sudoers',
+    changes => [
+      'set spec[user = "%wheel"]/user %wheel',
+      'set spec[user = "%wheel"]/host_group/host ALL',
+      'set spec[user = "%wheel"]/host_group/command[1] ALL',
+      'set spec[user = "%wheel"]/host_group/command[1]/tag NOPASSWD',
+    ]
+  }
 
 #===========================================
 # Disable password auth, key auth only
