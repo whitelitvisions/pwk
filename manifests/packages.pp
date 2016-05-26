6ee7894f098b32761dce23a3e26711cd2f6c5ce4
michaeldkelly <michaeldkelly@gmail.com>
2016-05-26 00:43:20 -0400

    Adding passenger repo

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -8,31 +8,65 @@
 class pwk::packages {
 
 #===========================================
-# Ensure rubygems so we can use gem provider
+# Ensure repos
+#===========================================
+$passenger_repo = "[passenger]
+name=passenger
+baseurl=https://oss-binaries.phusionpassenger.com/yum/passenger/el/$releasever/$basearch
+repo_gpgcheck=1
+gpgcheck=0
+enabled=1
+gpgkey=https://packagecloud.io/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+
+[passenger-source]
+name=passenger-source
+baseurl=https://oss-binaries.phusionpassenger.com/yum/passenger/el/$releasever/SRPMS
+repo_gpgcheck=1
+gpgcheck=0
+enabled=1
+gpgkey=https://packagecloud.io/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+"
+  file { '/etc/yum.repos.d/passenger.repo'
+    mode     => '0600',
+    owner    => 'root',
+    group    => 'root',
+    before   => Package['mod_passenger'],
+    content  => $passenger_repo
+
+  }
+  package { 'epel-release':
+    ensure   => installed
+  }
+#===========================================
+# Ensure packages we need
 #===========================================
   package { 'rubygems':
-    ensure => present,
+    ensure   => present
+  }
+  package { 'mod_passenger':
+    ensure   => installed,
   }
 #===========================================
 # Install packages i care about :)
 #===========================================
   package { 'vim-enhanced':
-    ensure  => installed,
+    ensure   => installed
   }
   package { 'mlocate':
-    ensure  => installed,
+    ensure   => installed
   }
   package { 'tree':
-    ensure  => installed,
+    ensure   => installed
   }
   package { 'puppet-lint':
-    ensure  => installed,
-    provider => gem,
+    ensure   => installed,
+    provider => gem
   }
   package { 'git':
-    ensure  => installed,
-  }
-  package { 'epel-release':
-    ensure => installed,
+    ensure   => installed
   }
 }
